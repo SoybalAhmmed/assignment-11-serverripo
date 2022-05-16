65dcda5cc57078d4f6c444f6f5769395568f9807
SoybalAhmmed <soybalahmed@gmail.com>
2022-05-16 23:07:28 +0600

    manage added

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ async function run() {
             const result = await bookCollection.insertOne(newBook);
             res.send(result);
         });
+
+        app.delete('/book/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await bookCollection.deleteOne(query);
+            res.send(result);
+        });
      
     }
     finally {
